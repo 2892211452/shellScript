4ccad2b93a88ef789a13630378b85f12029fd7e9
2892211452 <2892211452@qq.com>
2020-04-25 13:30:01 +0800

    监视文件

--- fileJS.sh
+++ fileJS.sh
@@ -1,6 +1,6 @@
 function whether_changed(){
-    local file_path='/home/lwl/Study/latex/test.tex'
-    local check_time=2
+    local file_path='/home/lwl/Study/latex/test.tex' #要监视文件的路径
+    local check_time=2 #等待时间
     while [[ true ]]; do
         file_old_stat="`stat ${file_path}`"
         sleep ${check_time}
@@ -12,6 +12,7 @@ function whether_changed(){
             #没有改变
         else
             #echo "#### Wait ${check_time}s ####"
+            #紧接需要执行的任务
             xelatex test.tex
         fi
     done
